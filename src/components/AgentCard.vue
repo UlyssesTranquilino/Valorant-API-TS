6cf2daf94a0e335ec09edbe7df79c5f3b6ad0fac
Ulysses <tranquilinoulysses9@gmail.com>
2024-11-22 23:02:44 +0800

    quasar work 1

---
+++ src/components/AgentCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="text-white">NAME PLACEHOLDER</h1>
+  </div>
+</template>
+
+<script lang="ts"></script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+}
+</style>
---
+++ src/components/Search.vue
@@ -0,0 +1,60 @@
+<template>
+  <div
+    class="search-container q-pt-xl q-pb-lg flex items-center justify-center text-white"
+  >
+    <div class="row items-center justify-center">
+      <p class="q-ma-xs col-12 text-center">
+        A 5v5 character-based tactical shooter
+      </p>
+      <h1 class="valo-title q-ma-xs col-12 text-center">VALORANT</h1>
+    </div>
+    <div class="q-pa-xs q-ma-xs search flex items-center justify-center row">
+      <q-input filled v-model="text" :dense="dense" class="bg-white q-mt-xs">
+        <template v-slot:prepend>
+          <q-icon name="search"></q-icon>
+        </template>
+      </q-input>
+      <div class="search-button-container q-mt-lg">
+        <q-btn
+          square
+          style="color: white"
+          label="SEARCH AGENT"
+          class="bg-primary searh-button q-ma-xs"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const dense = ref<string>("dense");
+const text = ref<string>("");
+</script>
+
+<style scoped>
+@import url("https://fonts.cdnfonts.com/css/valorant");
+
+.search-container {
+  background-image: url("../assets/Background/SearchBg.png");
+  background-size: cover;
+  height: 400px;
+  background-position: center;
+}
+
+.search {
+  width: 250px;
+  height: 35px;
+  transform: translateY(-90px);
+}
+
+.valo-title {
+  font-family: "Valorant";
+  font-size: 50px;
+}
+
+.search-button-container {
+  border: 1px solid white;
+}
+</style>
